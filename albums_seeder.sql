c773e404bbcff2379d01f23aad945b8d9bfb65e2
Jim Lochhead <lochhead71@gmail.com>
2016-03-14 15:02:36 -0500

    Got rid of the reduntant INPUT lines and "VALUES" lavel on each remaining line

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,66 +1,35 @@
 USE codeup_test_db;
 
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Michael Jackson', 'Thriller', 1982-01-01, 48.1, 'Pop, rock, R&B');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('AC/DC', 'Back in Black', 1980-01-01, 25.9, 'Hard rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973-01-01, 22.7 , 'Progressive rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Whitney Houston', 'The Bodyguard', 1992-01-01, 27.4 , 'R&B, soul, pop');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Meat Loaf', 'Bat Out of Hell', 1977-01-01, 20.6, 'Hard rock, progressive rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', 1976-01-01, 32.2, 'Rock, soft rock, folk rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Bee Gees', 'Saturday Night Fever', 1977-01-01, 19, 'Disco');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Fleetwood Mac', 'Rumours', 1977-01-01, 27.9 , 'Soft rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Shania Twain', 'Come On Over', 1997-01-01, 29.6, 'Country, pop');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971-01-01, 29.0, 'Hard rock, heavy metal');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Michael Jackson', 'Bad', 1987-01-01, 20.3, 'Pop, funk, rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Alanis Morissette', 'Jagged Little Pill', 1995-01-01, 24.8, 'Alternative rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Celine Dion', 'Falling into You', 1996-01-01, 20.2 , 'Pop, Soft rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967-01-01, 13.1, 'Pop, Soft rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Eagles', 'Hotel California', 1976-01-01, 21.5, 'Rock, soft rock, folk rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Mariah Carey', 'Music Box', 1993-01-01, 19.0, 'Pop, R&B, Rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Michael Jackson', 'Dangerous', 1991-01-01, 17.6, 'Rock, Funk, Pop');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Patrick Swayze', 'Dirty Dancing', 1987-01-01, 17.9, 'Pop, rock, R&B');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Celine Dion', 'Let''s Talk About Love', 1997-01-01, 19.3, 'Pop, Soft rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('The Beatles', '1', 2000-01-01, 21.6, 'Rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Adele', '21', 2011-01-01, 22.3, 'Pop, soul');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('The Beatles', 'Abbey Road', 1969-01-01, 24.4, 'Rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984-01-01, 19.6, 'Rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Dire Straits', 'Brothers in Arms', 1985-01-01, 17.7, 'Rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Whitney Houston', 'Whitney Houston', 1985-01-01, 17.7, 'Pop, R&B');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('James Horner', 'Titanic: Music from the Motion Picture', 1997-01-01, 18.1, 'Soundtrack');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Madonna', 'The Immaculate Collection', 1990-01-01, 19.4, 'Pop, dance');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Metallica', 'Metallica', 1991-01-01, 19.9, 'Thrash metal, heavy metal');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Nirvana', 'Nevermind', 1991-01-01, 16.7, 'Grunge, alternative rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Pink Floyd', 'The Wall', 1979-01-01, 17.6, 'Progressive rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Santana', 'Supernatural', 1999-01-01, 20.5, 'Rock');
-INSERT INTO albums ('artist', 'name', 'release_date', 'sales', 'genre')
-VALUES ('Guns N'' Roses', 'Appetite for Destruction', 1987-01-01, 21.3, 'Heavy metal, hard rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982-01-01, 48.1, 'Pop, rock, R&B'),
+('AC/DC', 'Back in Black', 1980-01-01, 25.9, 'Hard rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973-01-01, 22.7 , 'Progressive rock'),
+('Whitney Houston', 'The Bodyguard', 1992-01-01, 27.4 , 'R&B, soul, pop'),
+('Meat Loaf', 'Bat Out of Hell', 1977-01-01, 20.6, 'Hard rock, progressive rock'),
+('Eagles', 'Their Greatest Hits (1971–1975)', 1976-01-01, 32.2, 'Rock, soft rock, folk rock'),
+('Bee Gees', 'Saturday Night Fever', 1977-01-01, 19, 'Disco'),
+('Fleetwood Mac', 'Rumours', 1977-01-01, 27.9 , 'Soft rock'),
+('Shania Twain', 'Come On Over', 1997-01-01, 29.6, 'Country, pop'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971-01-01, 29.0, 'Hard rock, heavy metal'),
+('Michael Jackson', 'Bad', 1987-01-01, 20.3, 'Pop, funk, rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995-01-01, 24.8, 'Alternative rock'),
+('Celine Dion', 'Falling into You', 1996-01-01, 20.2 , 'Pop, Soft rock'),
+('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967-01-01, 13.1, 'Pop, Soft rock'),
+('Eagles', 'Hotel California', 1976-01-01, 21.5, 'Rock, soft rock, folk rock'),
+('Mariah Carey', 'Music Box', 1993-01-01, 19.0, 'Pop, R&B, Rock'),
+('Michael Jackson', 'Dangerous', 1991-01-01, 17.6, 'Rock, Funk, Pop'),
+('Patrick Swayze', 'Dirty Dancing', 1987-01-01, 17.9, 'Pop, rock, R&B'),
+('Celine Dion', 'Let''s Talk About Love', 1997-01-01, 19.3, 'Pop, Soft rock'),
+('The Beatles', '1', 2000-01-01, 21.6, 'Rock'),
+('Adele', '21', 2011-01-01, 22.3, 'Pop, soul'),
+('The Beatles', 'Abbey Road', 1969-01-01, 24.4, 'Rock'),
+('Bruce Springsteen', 'Born in the U.S.A.', 1984-01-01, 19.6, 'Rock'),
+('Dire Straits', 'Brothers in Arms', 1985-01-01, 17.7, 'Rock'),
+('Whitney Houston', 'Whitney Houston', 1985-01-01, 17.7, 'Pop, R&B'),
+('James Horner', 'Titanic: Music from the Motion Picture', 1997-01-01, 18.1, 'Soundtrack'),
+('Madonna', 'The Immaculate Collection', 1990-01-01, 19.4, 'Pop, dance'),
+('Metallica', 'Metallica', 1991-01-01, 19.9, 'Thrash metal, heavy metal'),
+('Nirvana', 'Nevermind', 1991-01-01, 16.7, 'Grunge, alternative rock'),
+('Pink Floyd', 'The Wall', 1979-01-01, 17.6, 'Progressive rock'),
+('Santana', 'Supernatural', 1999-01-01, 20.5, 'Rock'),
+('Guns N'' Roses', 'Appetite for Destruction', 1987-01-01, 21.3, 'Heavy metal, hard rock');
