773066476b93fef41083e1f1e1737dcfe8fe6bcc
Jim Lochhead <lochhead71@gmail.com>
2016-03-15 14:52:58 -0500

    Combined the hired in the 90's and Christmas birthday queries with an AND conditional

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,15 +8,17 @@ AND gender ="M";
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%'
+OR last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
 AND last_name LIKE '%e';
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
-
-SELECT *
-FROM employees
-WHERE birth_date LIKE '%-12-25';
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
