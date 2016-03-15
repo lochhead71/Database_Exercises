3aefc2df05aa3a4ddf853602705c2abf4769165c
Jim Lochhead <lochhead71@gmail.com>
2016-03-15 14:46:19 -0500

    Replace IN statement with chained IF's

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,16 @@
 SELECT *
 FROM employees
-WHERE first_name IN('Irena', 'Vidya', 'Maya');
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
 
 SELECT *
 FROM employees
