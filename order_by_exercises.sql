77e8c8cd176ede14714d512e9797fe7a1bf04725
Jim Lochhead <lochhead71@gmail.com>
2016-03-15 15:32:40 -0500

    Changed the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -20,7 +20,8 @@ AND last_name LIKE '%e';
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT *
 FROM employees
