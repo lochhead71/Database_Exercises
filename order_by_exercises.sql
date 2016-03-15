19e521ff49ed461f1e5f072b1ca0e78e14e480bb
Jim Lochhead <lochhead71@gmail.com>
2016-03-15 15:29:44 -0500

    Reversed the sort order of both queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,12 +4,13 @@ WHERE (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya')
 AND gender ="M"
-ORDER BY last_name asc, first_name asc;
+ORDER BY last_name desc, first_name desc;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%'
-OR last_name LIKE '%e';
+OR last_name LIKE '%e'
+ORDER BY emp_no desc;
 
 SELECT *
 FROM employees
