9991f88d6bcd3677af5dcc07d066d2a9685ba858
Jim Lochhead <lochhead71@gmail.com>
2016-03-15 16:57:38 -0500

    Completed the third step of the exercise

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -2,8 +2,6 @@ SELECT title
 FROM titles 
 GROUP BY title ASC;
 
-SELECT last_name
+SELECT DISTINCT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'e%'
-AND last_name LIKE '%e'
-GROUP BY last_name;
+WHERE last_name LIKE 'e%e';
