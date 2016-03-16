cf95591855a8a5103a3f6e1f446451c5d2b59961
Jim Lochhead <lochhead71@gmail.com>
2016-03-16 10:19:48 -0500

    Used count(*) and GROUP BY to find the number of employees for each gender with the first_name of 'Irena', 'Vidya', or 'Maya'.

---
+++ functions_exercises.sql
@@ -0,0 +1,28 @@
+SELECT COUNT(first_name), gender
+FROM employees
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+GROUP BY gender;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+OR last_name LIKE '%e'
+ORDER BY emp_no desc;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+AND last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
