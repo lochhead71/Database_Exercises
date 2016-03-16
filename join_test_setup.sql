0316eb182ca4d266e665403c6093a2ed842cd7d8
Jim Lochhead <lochhead71@gmail.com>
2016-03-16 15:12:15 -0500

    Ran JOIN, LEFT JOIN and RIGHT JOIN on join_test_db's two tables and got different results

--- join_test_setup.sql
+++ join_test_setup.sql
@@ -31,3 +31,15 @@ INSERT INTO users (name, email, role_id) VALUES
 ('luis', 'luis@example.com', 2),
 ('pascal', 'thomas@example.com', 2),
 ('cam', 'camerson@example.com', NULL);
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id
